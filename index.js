e988d73888311fbf77efbfdb92d1371c2792b562
Brian Kubes <briankubes@gmail.com>
2020-10-29 10:00:50 -0700

    working

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+require('dotenv').config()
+
 const server = require("./api/server.js");
 
-const port = 5000;
+
+
+const port = process.env.PORT
+const secret = process.env.SECRET_THING || 'bar'
+console.log(port, secret)
+
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
